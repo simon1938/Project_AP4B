4d3345dbb7a7361e72435493cf4faeb4801f63e4
simon <simon.illouz--laurent@utbm.fr>
2023-05-22 23:26:26 +0200

    test du commit

--- src/project/ap4a/graph/Graph.java
+++ src/project/ap4a/graph/Graph.java
@@ -1,7 +1,8 @@
 package project.ap4a.graph;
 
 public class Graph {
-    public static void main(){
-        System.out.println("hello world ");
+    public static void main(String[] args){
+        System.out.println("hello world hehehehe");
+
     }
 }
